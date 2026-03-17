960a6eb3bfef24a6258e0338033241c85ef7cd58
Marina Crespo <marina1@uni-bremen.de>
2026-03-17 20:26:14 +0100

    new model

---
+++ model/V5/bird_detector_model_data.h
@@ -0,0 +1,7 @@
+#ifndef BIRD_DETECTOR_MODEL_DATA_H_
+#define BIRD_DETECTOR_MODEL_DATA_H_
+
+extern const unsigned char bird_detector_model[];
+extern const int bird_detector_model_len;
+
+#endif  // BIRD_DETECTOR_MODEL_DATA_H_
